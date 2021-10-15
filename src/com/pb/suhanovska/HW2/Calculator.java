86ace56dd7e5afa0b5ca52bd01ae447ec9c8d589
Olena Suhanovska <suhanovska11@gmail.com>
2021-10-15 15:50:18 +0300

    init commit

--- src/com/pb/suhanovska/HW2/Calculator.java
+++ src/com/pb/suhanovska/HW2/Calculator.java
@@ -4,4 +4,59 @@ import java.util.Scanner;
 
 public class Calculator {
 
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int operand1 = getInt();
+        int operand2 = getInt();
+        char operation = getOperation();
+        int result = calc(operand1,operand2,operation);
+        System.out.println("Результат операции: "+result);
+}
+    public static int getInt(){
+        System.out.println("Введите число:");
+        int num;
+        if(scanner.hasNextInt()){
+            num = scanner.nextInt();
+        } else {
+            System.out.println("Вы допустили ошибку при вводе числа. Попробуйте еще раз.");
+            scanner.next();
+            num = getInt();
+        }
+        return num;
+    }
+    public static char getOperation(){
+        System.out.println("Введите операцию:");
+        char operation;
+        if(scanner.hasNext()){
+            operation = scanner.next().charAt(0);
+        } else {
+            System.out.println("Вы допустили ошибку при вводе операции. Попробуйте еще раз.");
+            scanner.next();
+            operation = getOperation();
+        }
+        return operation;
+    }
+
+    public static int calc(int operand1, int operand2, char operation){
+        int result;
+        switch (operation){
+            case '+':
+                result = operand1+operand2;
+                break;
+            case '-':
+                result = operand1-operand2;
+                break;
+            case '*':
+                result = operand1*operand2;
+                break;
+            case '/':
+                result = operand1/operand2;
+                break;
+            default:
+                System.out.println("Операция не распознана. Повторите ввод.");
+                result = calc(operand1, operand2, getOperation());
+        }
+        return result;
+    }
 }
